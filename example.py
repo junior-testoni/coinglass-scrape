cf557cba83ba012a7fb47d36ad57267c9c2fae96
junior-testoni <82902129+junior-testoni@users.noreply.github.com>
2025-05-21 00:22:26 -0400

    Create example.py

---
+++ example.py
@@ -0,0 +1,13 @@
+#get suported coins 
+import requests
+
+url = "https://open-api-v4.coinglass.com/api/futures/supported-coins"
+
+headers = {
+    "accept": "application/json",
+    "CG-API-KEY": "53b0a3236d8d4d2b9fff517c70c544ea"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.text)
