5e4a1e91ac59b3f01c8892399caf2bfe79cb00b3
junior-testoni <82902129+junior-testoni@users.noreply.github.com>
2025-05-22 00:04:33 -0400

    Fix example script

--- example.py
+++ example.py
@@ -1,14 +1,15 @@
-
-#get suported coins 
+# get supported coins
 import requests
 
 url = "https://open-api-v4.coinglass.com/api/futures/supported-coins"
 
 headers = {
     "accept": "application/json",
-    "CG-API-KEY": "53b0a3236d8d4d2b9fff517c70c544ea"
+    "CG-API-KEY": "53b0a3236d8d4d2b9fff517c70c544ea",
 }
 
 response = requests.get(url, headers=headers)
-
-print(response.text)
+if response.status_code != 200:
+    print(f"Request failed: {response.status_code}")
+else:
+    print(response.text)
