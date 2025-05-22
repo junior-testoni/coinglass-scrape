9e584b1c2419beae9346e1b00d7a97241fa8a789
junior-testoni <82902129+junior-testoni@users.noreply.github.com>
2025-05-21 20:15:36 -0400

    a
    a

--- wss.js
+++ wss.js
@@ -1,6 +1,5 @@
 const wssWebSocket = new WebSocket("wss://open-ws.coinglass.com/ws-api?cg-api-key={53b0a3236d8d4d2b9fff517c70c544ea}");
 console.log(wssWebSocket.url); // 'wss://websocket.example.org'
 
-// Do something with socket
 
 wssWebSocket.close();
